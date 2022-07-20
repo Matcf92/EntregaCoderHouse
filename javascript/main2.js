e1766ee177833d649d418ddb556339a41d4f9eac
Mathias Fagundez <mathifs92@gmail.com>
2022-07-19 22:06:48 -0300

    Martes

--- javascript/main2.js
+++ javascript/main2.js
@@ -75,23 +75,58 @@ iniciarSesion2 = () => {
 
 const boton1 = document.getElementById("btn1");
 boton1.addEventListener("click", () => {
-    console.log("Imprimiendo entrada para: Dog Un viaje salvaje")
+    Swal.fire(
+        'Imprimiendo entrada...',
+        'Que disfrutes la pelicula!',
+        'success'
+    )
 });
 
 const boton2 = document.getElementById("btn2");
 boton2.addEventListener("click", () => {
-    console.log("Imprimiendo entrada para: Doctor Strange")
+    Swal.fire(
+        'Imprimiendo entrada...',
+        'Que disfrutes la pelicula!',
+        'success'
+    )
 });
 
 const boton3 = document.getElementById("btn3");
 boton3.addEventListener("click", () => {
-    console.log("Imprimiendo entrada para: Star Wars")
+    Swal.fire(
+        'Imprimiendo entrada...',
+        'Que disfrutes la pelicula!',
+        'success'
+    )
 });
 
 const boton4 = document.getElementById("btn4");
 boton4.addEventListener("click", () => {
-    botonPeliculas();
+    Swal.fire(
+        'Imprimiendo entrada...',
+        'Que disfrutes la pelicula!',
+        'success'
+    )
 });
+
+const boton5 = document.getElementById("btn5");
+boton5.addEventListener("click", () => {
+    Swal.fire(
+        'Imprimiendo entrada...',
+        'Que disfrutes la pelicula!',
+        'success'
+    )
+});
+
+const boton6 = document.getElementById("btn6");
+boton6.addEventListener("click", () => {
+    Swal.fire(
+        'Imprimiendo entrada...',
+        'Que disfrutes la pelicula!',
+        'success'
+    )
+});
+
 
 function hacerPedido() {
     let comida = input1.value
@@ -104,8 +139,53 @@ function hacerPedido() {
     count++
 }
 
-function botonPeliculas(){
-    fetch("../data/data.json")
-    .then((res)=>res.json())
-    .then((json)=>console.log(json))
+let pagina = 1;
+const btnAnterior = document.getElementById('btnAnterior');
+const btnSiguiente = document.getElementById('btnSiguiente');
+
+btnSiguiente.addEventListener('click', () => {
+    if (pagina < 10) {
+        pagina += 1;
+        cargarPeliculas();
+    }
+});
+
+btnAnterior.addEventListener('click', () => {
+    if (pagina > 1) {
+        pagina -= 1;
+        cargarPeliculas();
+    }
+});
+
+const cargarPeliculas = async () => {
+    try {
+        const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=007deb9b3e90a81dc4be5eb21d7cfc6b&language=es-MX&page=${pagina}`);
+
+        console.log(respuesta);
+
+        // Si la respuesta es correcta
+        if (respuesta.status === 200) {
+            const datos = await respuesta.json();
+
+            let peliculas = '';
+            datos.results.forEach(pelicula => {
+                peliculas += `
+					<div class="pelicula">
+						<img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+						<h3 class="titulo">${pelicula.title}</h3>
+					</div>
+				`;
+            });
+
+            document.getElementById('contenedor').innerHTML = peliculas;
+
+        } else {
+            console.log('Hubo un error y no sabemos que paso');
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+
 }
+
